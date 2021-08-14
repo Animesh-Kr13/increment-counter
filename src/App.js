e52472149acb5e19268aaa5e4e3217f833219277
Animesh-Kr13 <kumar.animesh2759@gmail.com>
2021-08-14 15:32:14 +0530

    made an increment counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(prevCount => prevCount + 1)
+  }
+  
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>Increment</button>
+    </div>
   );
 }
 
